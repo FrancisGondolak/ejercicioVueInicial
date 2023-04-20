89f0120ce183e0e8a898a367291c1013168863d9
FrancisNeoris <francisco.albiar@hotmail.com>
2023-04-20 17:31:07 +0200

    Insertado un Method y unos if en el formulario

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,22 @@
 <template>
     <section class="v-home">
        <div class="v-home__form">
-            <h2>{{ username }}</h2>
-            <h2>{{ password }}</h2>
+            <p class="v-home__form__title">Introduce tu datos</p>
             <CInput
             type="text" 
             placeholder="Introduce el usuario" 
             v-model="username"
             />
+            <p v-if="!userCorrect">El usuario ha de tener un mínimo de 6 caracteres</p>
             <CInput
             type="password" 
             placeholder="Introduce el password" 
             v-model="password"
             />
+            <p v-if="!passwordCorrect">La contraseña ha de tener un caracter especial</p>
             <CButton
             buttonMessage="Login"
+            @click=buttonPush()
             ></CButton>
         </div>  
     </section>
@@ -35,7 +37,8 @@ export default {
         return {
             username: "",
             password: "",
-            userCorrect: false
+            userCorrect: false,
+            passwordCorrect: false
         }
     },
     watch: {
@@ -43,10 +46,18 @@ export default {
             if(value === "Francisco"){
                 this.userCorrect = true;
             }
-        } 
+        },
+        password(value) {
+            if(value === "@Neoris"){
+                this.passwordCorrect = true;
+            }
+        }
+
     },
     methods: {
-        
+        buttonPush() {
+            alert("Botón funciona")
+        }     
     }
 };
 </script>
@@ -59,6 +70,7 @@ export default {
     flex-direction: column;
     justify-content: center;
     align-items: center;
+    height: 600px;
 }
 
 .v-home__form{
@@ -68,9 +80,14 @@ export default {
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    height: 100%;
+    height: 70%;
     width: 40%;
     background: var(--color-background-form);
+}
+
+.v-home__form__title {
+    color: var(--color-title-text);
+    font-size: 35px;
 }
 
 
