6f0cf7b56d92d4c7bb1b884b07ead6d6f5f348da
FrancisNeoris <francisco.albiar@hotmail.com>
2023-05-08 13:35:33 +0200

    Componenten c-warning terminado
    Agregado correctamente al Login

--- src/components/c-warning.vue
+++ src/components/c-warning.vue
@@ -1,14 +1,22 @@
 <template>
     <div class="c-warning">
         <img class="c-warning__image" src="src/components/assets/images/professorOak.png" alt="professorOakImage">
-        <button @click="dismissWarning">Aceptar</button>
+        <CButton
+            buttonMessage="Aceptar"
+            @click=dismissWarning
+        ></CButton>
     </div>
 </template>
 
 <script>
 
+import CButton from "@/components/c-button.vue"
+
 export default {
     name: "c-warning",
+    components: {
+        CButton
+    },
     props: {
     },
     data() {
@@ -29,21 +37,19 @@ export default {
 <style lang="scss" scoped>
 
 .c-warning {
-    background-color: red;
+    // background-color: var(--color-form-background);
+    background-color: var(--color-form-background);
     width: 40%;
     height: 100%;
     display: flex;
     flex-direction: column;
     align-items: center;
+    justify-content: center;
 }
 
 .c-warning__image {
-    width: 80%;
-    height: 80%;
-}
-
-button {
-   width: 30%;
+    width: 40%;
+    height: 50%;
 }
 
 </style>
