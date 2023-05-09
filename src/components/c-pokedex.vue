0ec492d59c80cde728a5744eb2574fc51deef303
FrancisNeoris <francisco.albiar@hotmail.com>
2023-05-09 10:14:40 +0200

    Añadido icono al botón de apagar Pokédex
    Además he hecho un poco más responsives, con porcentajes, los botones de la Pokédex

--- src/components/c-pokedex.vue
+++ src/components/c-pokedex.vue
@@ -1,272 +1,288 @@
 <template>
-    <div class="c-pokedex">
-      <audio autoplay src="src/components/assets/audio/openPokedex.mp3"></audio>
-        <section class="c-pokedex__top">
-          <div class="big_circle_blue"></div>
-          <div class="small_circle_red"></div>
-          <div class="small_circle_orange"></div>
-          <div class="small_circle_green"></div>
-        </section>
-        <section class="c-pokedex__middle">
-          <section class="c-pokedex__middle--top">
-            <span class="c-pokedex--name">{{ pokemonshown.name }}</span>
-            <audio autoplay :src=getSound(pokemonshown.name) v-if="playAudio"></audio>
-          </section>
-          <section class="c-pokedex__middle--bottom">
-            <div class="c-pokedex__image">
-              <img class="pokeImage" :src="pokemonshown.image" :alt="'imagen' + pokemonshown.name">
-            </div>  
-            <div class="c-pokedex__info">
-              <span class="c-pokedex--type"><b>Tipo: </b>{{ pokemonshown.type }}</span>
-              <span class="c-pokedex--number"><b>Número: </b>{{ pokemonshown.number }}</span>
-              <span class="c-pokedex--weight"><b>Peso: </b>{{ pokemonshown.weight }}</span>
-              <span class="c-pokedex--height"><b>Altura: </b>{{ pokemonshown.height }}</span>
-              <span class="c-pokedex--description"><b>Descripción: </b>{{ pokemonshown.description }}</span>
-            </div>
-          </section>  
-        </section>
-        <section class="c-pokedex__bottom">
-          <section class="c-pokedex__bottom--top">
-            <div class="medium_circle_darkblue" @click=playingAudio></div>
-            <div class="medium_bar_green"></div>
-            <div class="medium_bar_yellow"></div>
-          </section>
-          <section class="c-pokedex__bottom--bottom">
-            <div class="long_bar_yellow"></div>
-            <div class="medium_circle_red" @click=closePokedex></div>
-          </section>
-        </section>
-    </div>
+  <div class="c-pokedex">
+    <audio autoplay src="src/components/assets/audio/openPokedex.mp3"></audio>
+    <section class="c-pokedex__top">
+      <div class="big_circle_blue"></div>
+      <div class="small_circle_red"></div>
+      <div class="small_circle_orange"></div>
+      <div class="small_circle_green"></div>
+    </section>
+    <section class="c-pokedex__middle">
+      <section class="c-pokedex__middle--top">
+        <span class="c-pokedex--name">{{ pokemonshown.name }}</span>
+        <audio autoplay :src="getSound(pokemonshown.name)" v-if="playAudio"></audio>
+      </section>
+      <section class="c-pokedex__middle--bottom">
+        <div class="c-pokedex__image">
+          <img class="pokeImage" :src="pokemonshown.image" :alt="'imagen' + pokemonshown.name" />
+        </div>
+        <div class="c-pokedex__info">
+          <span class="c-pokedex--type"><b>Tipo: </b>{{ pokemonshown.type }}</span>
+          <span class="c-pokedex--number"><b>Número: </b>{{ pokemonshown.number }}</span>
+          <span class="c-pokedex--weight"><b>Peso: </b>{{ pokemonshown.weight }}</span>
+          <span class="c-pokedex--height"><b>Altura: </b>{{ pokemonshown.height }}</span>
+          <span class="c-pokedex--description"
+            ><b>Descripción: </b>{{ pokemonshown.description }}</span
+          >
+        </div>
+      </section>
+    </section>
+    <section class="c-pokedex__bottom">
+      <section class="c-pokedex__bottom--top">
+        <div class="medium_circle_darkblue" @click="playingAudio"></div>
+        <div class="medium_bar_green"></div>
+        <div class="medium_bar_yellow"></div>
+      </section>
+      <section class="c-pokedex__bottom--bottom">
+        <div class="long_bar_yellow"></div>
+        <div class="medium_circle_red" @click="closePokedex">
+          <img
+            class="turnOffIcon"
+            src="src/components/assets/icons/turnOff.svg"
+            alt="turnOffIcon"
+          />
+        </div>
+      </section>
+    </section>
+  </div>
 </template>
 
 <script>
-
 export default {
-    name: "c-pokedex",
-    props: {
-        pokemonshown: {
-            type: Object,
-            required: true
-        }
-    },
-    data() {
-      return {
-        selected: false,
-        playAudio: false
-      }
-    },
-    methods: {
-      getColor() {
-        return "background-color: var(--color-PokemonType--" + this.pokemonshown.type +");";
-      },
-      getSound(name) {
-        return "src/components/assets/audio/" + name + "Sound.mp3"
-      },
-      //función para reproducir el audio al pulsar sobre el botón darkblue de la Pokédex. Creamos dentro 
-      //una función setTimeout para hacer que ponga en false al cabo de 100 ms la variable playAudio y
-      //así poder repetir el audio al pulsar sobre el botón
-      playingAudio() {
-        setTimeout(() => {this.playAudio = true}, 100)
-        this.playAudio = false
-      },
-      closePokedex() {
-        this.$emit('pokemonUnselected', this.selected);  
-      }
+  name: 'c-pokedex',
+  props: {
+    pokemonshown: {
+      type: Object,
+      required: true
     }
-
+  },
+  data() {
+    return {
+      selected: false,
+      playAudio: false
+    }
+  },
+  methods: {
+    getColor() {
+      return 'background-color: var(--color-PokemonType--' + this.pokemonshown.type + ');'
+    },
+    getSound(name) {
+      return 'src/components/assets/audio/' + name + 'Sound.mp3'
+    },
+    //función para reproducir el audio al pulsar sobre el botón darkblue de la Pokédex. Creamos dentro
+    //una función setTimeout para hacer que ponga en false al cabo de 100 ms la variable playAudio y
+    //así poder repetir el audio al pulsar sobre el botón
+    playingAudio() {
+      setTimeout(() => {
+        this.playAudio = true
+      }, 100)
+      this.playAudio = false
+    },
+    closePokedex() {
+      this.$emit('pokemonUnselected', this.selected)
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+.c-pokedex {
+  border: 5px solid black;
+  border-radius: 15px;
+  margin-top: 20px;
+  height: 600px;
+  width: 100%;
+  padding: 5px 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  box-shadow: var(--color-shadow-boxPokemon);
+  background-color: var(--color-backgroundPokedex);
+}
 
-  .c-pokedex {
-    border: 5px solid black;
-    border-radius: 15px;
-    margin-top: 20px;
-    height: 600px;
-    width: 100%;
-    padding: 5px 0;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    box-shadow: var(--color-shadow-boxPokemon);
-    background-color: var(--color-backgroundPokedex);
-  }
+.c-pokedex__top {
+  width: 100%;
+  height: 140px;
+  margin-bottom: 15px;
+  display: flex;
+  flex-direction: row;
+}
 
-  .c-pokedex__top {
-    width: 100%;
-    margin-bottom: 15px;
-    display: flex;
-    flex-direction: row;
-  }
+.big_circle_blue {
+  border: var(--color-border-elementsPokedex);
+  background: rgb(50, 184, 224);
+  border-radius: 50%;
+  width: 15%;
+  height: 100%;
+  margin-top: 5px;
+  margin-left: 5px;
+}
 
-  .big_circle_blue {
-    border: var(--color-border-elementsPokedex);
-    background: rgb(50, 184, 224);
-    border-radius: 50%;
-    width: 70px;
-    height: 70px;
-    margin-top: 5px;
-    margin-left: 5px;
-  }
+.small_circle_red {
+  border: var(--color-border-elementsPokedex);
+  background: rgb(255, 17, 0);
+  border-radius: 50%;
+  width: 7%;
+  height: 50%;
+  margin-top: 5px;
+  margin-left: 5px;
+}
 
-  .small_circle_red {
-    border: var(--color-border-elementsPokedex);
-    background: rgb(255, 17, 0);
-    border-radius: 50%;
-    width: 35px;
-    height: 35px;
-    margin-top: 5px;
-    margin-left: 5px;
-  }
+.small_circle_orange {
+  border: var(--color-border-elementsPokedex);
+  background: rgb(231, 143, 12);
+  border-radius: 50%;
+  width: 7%;
+  height: 50%;
+  margin-top: 5px;
+  margin-left: 5px;
+}
 
-  .small_circle_orange {
-    border: var(--color-border-elementsPokedex);
-    background: rgb(231, 143, 12);
-    border-radius: 50%;
-    width: 35px;
-    height: 35px;
-    margin-top: 5px;
-    margin-left: 5px;
-  }
+.small_circle_green {
+  border: var(--color-border-elementsPokedex);
+  background: rgb(12, 231, 23);
+  border-radius: 50%;
+  width: 7%;
+  height: 50%;
+  margin-top: 5px;
+  margin-left: 5px;
+}
+.c-pokedex__middle--top {
+  display: flex;
+  height: 6%;
+  margin-left: auto;
+  margin-right: auto;
+  font-size: 28px;
+  font-weight: bold;
+  cursor: default;
+}
 
-  .small_circle_green {
-    border: var(--color-border-elementsPokedex);
-    background: rgb(12, 231, 23);
-    border-radius: 50%;
-    width: 35px;
-    height: 35px;
-    margin-top: 5px;
-    margin-left: 5px;
-  }
-  .c-pokedex__middle--top {
-    display: flex;
-    height: 6%;
-    margin-left: auto;
-    margin-right: auto;
-    font-size: 28px;
-    font-weight: bold;
-    cursor: default;
-  }
+.c-pokedex__middle--bottom {
+  display: flex;
+  height: 92%;
+}
 
-  .c-pokedex__middle--bottom {
-    display: flex;
-    height: 92%;
-  }
+.c-pokedex__middle {
+  width: 90%;
+  height: 100%;
+  border: var(--color-border-screenPokedex);
+  border-radius: 10px;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: space-around;
+  flex-direction: column;
+  background-color: rgb(255, 255, 255);
+}
 
-  .c-pokedex__middle {   
-    width: 90%;
-    height: 100%;
-    border: var(--color-border-screenPokedex);
-    border-radius: 10px;
-    box-sizing: border-box;
-    display: flex;
-    justify-content: space-around;
-    flex-direction: column;
-    background-color: rgb(255, 255, 255);
-  }
+.c-pokedex__image {
+  width: 50%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
 
-  .c-pokedex__image {
-    width: 50%;
-    height: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
+.pokeImage {
+  width: 90%;
+  height: 70%;
+}
 
-  .pokeImage {
-    width: 90%;
-    height: 70%;
-  }
+.c-pokedex__info {
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  align-items: left;
+  justify-content: space-evenly;
+  margin-right: 2px;
+  cursor: default;
+}
 
-  .c-pokedex__info {
-    width: 50%;
-    display: flex;
-    flex-direction: column;
-    align-items: left;
-    justify-content: space-evenly;
-    margin-right: 2px;
-    cursor: default;
-  }
+.c-pokedex__bottom {
+  height: 60%;
+  width: 100%;
+  margin-top: 15px;
+}
 
-  .c-pokedex__bottom {
-    height: 60%;
-    width: 100%;
-    margin-top: 15px;
-  }
+.c-pokedex__bottom--top {
+  width: 100%;
+  height: 45%;
+  display: flex;
+}
 
-  .c-pokedex__bottom--top {
-    width: 100%;
-    height: 45%;
-    display: flex;
-  }
+.medium_circle_darkblue {
+  border: var(--color-border-elementsPokedex);
+  background: rgb(0, 68, 255);
+  border-radius: 50%;
+  width: 10%;
+  height: 50px;
+  margin-top: 5px;
+  margin-left: 15px;
+  box-shadow: var(--color-shadow-buttonPokedex);
+  cursor: pointer;
+}
 
-  .medium_circle_darkblue {
-    border: var(--color-border-elementsPokedex);
-    background:  rgb(0, 68, 255);
-    border-radius: 50%;
-    width: 10%;
-    height: 50px;
-    margin-top: 5px;
-    margin-left: 15px;
-    box-shadow: var(--color-shadow-buttonPokedex);
-    cursor: pointer;
-  }
+.medium_circle_darkblue:hover {
+  transform: scale(0.9);
+}
 
-  .medium_circle_darkblue:hover {
-    transform: scale(0.9);
-  }
+.medium_bar_green {
+  border: var(--color-border-elementsPokedex);
+  background: rgb(26, 201, 20);
+  border-radius: 20px;
+  width: 25%;
+  height: 30px;
+  margin-top: 5px;
+  margin-left: 15px;
+}
 
-  .medium_bar_green {
-    border: var(--color-border-elementsPokedex);
-    background:  rgb(26, 201, 20);
-    border-radius: 20px;
-    width: 25%;
-    height: 30px;
-    margin-top: 5px;
-    margin-left: 15px;
-  }
+.medium_bar_yellow {
+  border: var(--color-border-elementsPokedex);
+  background: rgb(196, 199, 27);
+  border-radius: 20px;
+  width: 25%;
+  height: 30px;
+  margin-top: 5px;
+  margin-left: 15px;
+}
+.c-pokedex__bottom--bottom {
+  width: 100%;
+  height: 45%;
+  display: flex;
+}
 
-  .medium_bar_yellow {
-    border: var(--color-border-elementsPokedex);
-    background:  rgb(196, 199, 27);
-    border-radius: 20px;
-    width: 25%;
-    height: 30px;
-    margin-top: 5px;
-    margin-left: 15px;
-  }
-  .c-pokedex__bottom--bottom {
-    width: 100%;
-    height: 45%;
-    display: flex;
-  }
+.long_bar_yellow {
+  border: var(--color-border-elementsPokedex);
+  background: rgb(196, 199, 27);
+  border-radius: 20px;
+  width: 75%;
+  height: 60px;
+  margin-top: 5px;
+  margin-left: 15px;
+}
 
-  .long_bar_yellow {
-    border: var(--color-border-elementsPokedex);
-    background:  rgb(196, 199, 27);
-    border-radius: 20px;
-    width: 75%;
-    height: 60px;
-    margin-top: 5px;
-    margin-left: 15px;
-  }
+.medium_circle_red {
+  border: 5px solid black;
+  background: rgb(255, 8, 0);
+  box-shadow: var(--color-shadow-buttonPokedex);
+  border-radius: 50%;
+  width: 12%;
+  height: 60px;
+  margin-top: 5px;
+  margin-left: 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+}
 
-  .medium_circle_red {
-    border: 5px solid black;
-    background:  rgb(255, 8, 0);
-    box-shadow: var(--color-shadow-buttonPokedex);
-    border-radius: 50%;
-    width: 12%;
-    height: 60px;
-    margin-top: 5px;
-    margin-left: 15px;
-    cursor: pointer;
-  }
+.medium_circle_red:hover {
+  transform: scale(0.9);
+}
 
-  .medium_circle_red:hover {
-    transform: scale(0.9);
-  }
-
+.turnOffIcon {
+  width: 80%;
+  height: 80%;
+  cursor: pointer;
+}
 </style>
--- src/layouts/l-list.vue
+++ src/layouts/l-list.vue
@@ -1,76 +1,72 @@
 <template>
-    <div class="l-list">
-        <section class="l-listLeft">
-            <slot v-if="$slots.pokeballIcon" name="pokeballIcon" />
-            <CPokeball v-else></CPokeball>
-        </section>
-        <section class="l-listCenter">
-            <div class="l-listCenter__title">
-                <slot name="title" />
-            </div>
-            <div class="l-listCenter__nameList">
-                <slot name="pokemonList" />
-                <slot name="pokedex"></slot>
-            </div>
-        </section> 
-        <section class="l-listRight">
-            <slot v-if="$slots.pokeballIcon" name="pokeballIcon" />
-            <CPokeball v-else></CPokeball>
-        </section>
-    </div>
+  <div class="l-list">
+    <section class="l-listLeft">
+      <slot v-if="$slots.pokeballIcon" name="pokeballIcon" />
+      <CPokeball v-else></CPokeball>
+    </section>
+    <section class="l-listCenter">
+      <div class="l-listCenter__title">
+        <slot name="title" />
+      </div>
+      <div class="l-listCenter__nameList">
+        <slot name="pokemonList" />
+        <slot name="pokedex"></slot>
+      </div>
+    </section>
+    <section class="l-listRight">
+      <slot v-if="$slots.pokeballIcon" name="pokeballIcon" />
+      <CPokeball v-else></CPokeball>
+    </section>
+  </div>
 </template>
 
 <script>
 import CPokeball from '@/components/c-pokeball.vue'
 
 export default {
-    name:"LList",
+  name: 'LList',
 
-    components:{
-        CPokeball
-    }
+  components: {
+    CPokeball
+  }
 }
-
 </script>
 
 <style lang="scss" scoped>
-
 .l-list {
-    display: flex;
-    flex-direction: row;
+  display: flex;
 }
 
 .l-listLeft {
-    width: 30%;
-    height: 125px;
-    display: flex;
-    justify-content: right;
+  width: 30%;
+  height: 125px;
+  display: flex;
+  justify-content: right;
 }
 
 .l-listCenter {
-    width: 40%;
-    height: auto;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-    align-items: center;
+  width: 40%;
+  height: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
 }
 
 .l-listCenter__title {
-    margin-top: 5px;
-    margin-bottom: 5px;
-    text-align: center;
+  margin-top: 5px;
+  margin-bottom: 5px;
+  text-align: center;
 }
 
 .l-listCenter__nameList {
-    width: 60%;
+  width: 60%;
 }
 
 .l-listRight {
-    width: 30%;
-    height: 125px;
-    display: flex;
-    justify-content: left;
+  width: 30%;
+  height: 125px;
+  display: flex;
+  justify-content: left;
 }
-
 </style>
